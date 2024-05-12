c0f6a946146205351e2a71bf742d8536ea8441e4
Sarthak Kumar <kumar.v.sarthak@gmail.com>
2024-05-12 11:12:22 +0530

    1972/B

---
+++ coin_games.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        int n, ups = 0;
+        cin >> n;
+
+        for (int j = 0; j < n; j++)
+        {
+            char ch;
+
+            cin >> ch;
+            if (ch == 'U')
+                ups += 1;
+        }
+        if (ups % 2 != 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
